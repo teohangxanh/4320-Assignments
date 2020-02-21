6e15625d334df6d5fa08ed75b7603a3f4a4078b0
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-02-21 15:47:51 -0600

    .

--- Assignment2.py
+++ Assignment2.py
@@ -7,7 +7,7 @@ import pylab
 
 # I turned the rate into scores: Win = 3, Draw = 1, Lose = 0
 scores = [3, 3, 3, 3, 3, 3, 0, 3, 1, 1, 3, 0, 0, 3, 3, 0, 3, 3]
-# Assumption: The attendance before 3rd is the same: 8000
+# Assumption: The attendance before the 3rd is the same: 8000
 att = [8000] * len(scores)
 timesteps = []
 k=15000
@@ -34,8 +34,7 @@ for i in range(len(scores)):
         # LL
         elif game_before == 0:
             # Decrease geometrically
-            r = (r-1)*att[i-1]/(att[i-1]-k) + 1 - (r-1)*k/(att[i-1]-k)
-            att[i] = att[i-1] * (2-r)
+            att[i] = att[i-1] ** (0.9) # Since decay rate = 0.1 
             note = 'Decaying'
             
         # Other results
@@ -44,9 +43,8 @@ for i in range(len(scores)):
             att[i] = att[i-1] * (2-r)
             note = 'Decrease linearly'
     timesteps.append(i+1)
-    # print(note, end=' ')
-    # print(r, end=' ')
-    # print(att[i])
+    print(note, end=' ')
+    print(att[i])
       
 pylab.plot(timesteps, att)
 pylab.xlabel('Game')
